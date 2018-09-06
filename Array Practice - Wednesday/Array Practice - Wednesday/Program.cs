5c57c962a6bf72c3db8bb4a3b4038ffdd113f342
Amber Cook <30010451@student.toiohomai.ac.nz>
2018-09-06 14:39:20 +1200

    not complete

--- Array Practice - Wednesday/Array Practice - Wednesday/Program.cs
+++ Array Practice - Wednesday/Array Practice - Wednesday/Program.cs
@@ -32,5 +32,56 @@ namespace Array_Practice___Wednesday
                 Console.WriteLine($"Number {k}: {nums [k-1]}");
             }
         }
+
+        public static void task2()
+        {
+            //Task 2a:
+            Console.WriteLine("How many numbers would you like to store in the array?");
+            int[] array = new int[int.Parse(Console.ReadLine())];
+            
+            for (int i = 0; i<array.Length; i++)
+            {
+                Console.WriteLine($"Please enter number {i+1}: ");
+                array[i] = int.Parse(Console.ReadLine()); 
+            }
+            Console.ReadLine();
+            //FORWARD
+            int total = 0;
+            for (int j = 0; j < array.Length; j++)
+            {
+                Console.WriteLine($"Number {j + 1} (Array index {j}: num[j]");
+                total += array[j]; //'+=' is shortcut for 'total = total + array[j]';
+            }
+            Console.WriteLine("Total of these array items: " + total);
+
+            //REVERSE
+           
+            for (int j = array.Length; j < 0; j--)
+            {
+                Console.WriteLine($"Number {j - 1} (Array index {j}: num[j]");
+                total = array[j] + total;
+            }
+            Console.WriteLine("Total of these array items: " + total);
+
+            //Task 2b:
+            foreach (int x in array)
+            {
+                Console.WriteLine(x); //'int x in' - x can be anything, p, q, bob, etc. As long as it has int and in on either sides of it, it will work
+                total += x;
+            }
+            Console.WriteLine($"total = {total}");
+        }
+
+        public static void task3()
+        {
+            //Task 3:
+            int[] array1 = new int[5];
+            for (int i = 0; i < array1.Length; i++)
+            {
+                Console.WriteLine($"Please enter number {i + 1}: ");
+                array1[i] = int.Parse(Console.ReadLine());
+                
+            }
+        }
     }
 }
